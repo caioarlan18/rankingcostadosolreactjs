ac16725d4efe0865171791c268a0f9f6a6ee13d3
caioarlan18 <caioarlan58@gmail.com>
2023-05-30 20:20:00 -0300

    ajusting much things of score page

--- src/components/points/GeneralPoints.js
+++ src/components/points/GeneralPoints.js
@@ -3,149 +3,37 @@ import stylesDesktop from './GeneralPoints.desktop.module.css'
 import React, { useEffect } from 'react';
 import Header from '../Header/Header'
 import HeaderBottom from '../Header/HeaderBottom'
+import { useState } from 'react';
+
 function GeneralPoints() {
-    useEffect(() => {
-
-    })
-    var e11a = document.querySelector(`.${stylesMobile.tabela_1}`)
-    var e12a = document.querySelector(`.${stylesMobile.tabela_2}`)
-    var e13a = document.querySelector(`.${stylesMobile.tabela_3}`)
-    var e14a = document.querySelector(`.${stylesMobile.tabela_4}`)
-    var e15a = document.querySelector(`.${stylesMobile.tabela_5}`)
-    var e16a = document.querySelector(`.${stylesMobile.tabela_6}`)
-    var e17a = document.querySelector(`.${stylesMobile.tabela_7}`)
-
-    var e11b = document.querySelector(`.${stylesMobile.tabela2}`)
-    var e12b = document.querySelector(`.${stylesMobile.tabela3}`)
-    var e13b = document.querySelector(`.${stylesMobile.tabela4}`)
-    var e14b = document.querySelector(`.${stylesMobile.tabela5}`)
-    var e15b = document.querySelector(`.${stylesMobile.tabela6}`)
-    var e16b = document.querySelector(`.${stylesMobile.tabela7}`)
-    var e17b = document.querySelector(`.${stylesMobile.tabela8}`)
-
-    var oi1 = document.querySelector("div#oi1")
-    var oi2 = document.querySelector("div#oi2")
-    var oi3 = document.querySelector("div#oi3")
-    var oi4 = document.querySelector("div#oi4")
-    var oi5 = document.querySelector("div#oi5")
-    var oi6 = document.querySelector("div#oi6")
-
-    function e1a() {
-        e17a.classList.toggle(`${stylesMobile.bl}`)
-        e17b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.remove(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.none}`)
-        oi1.classList.toggle(`${stylesMobile.ordem}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    const [etapa6, setEtapa6] = useState(false)
+    const [etapa5, setEtapa5] = useState(false)
+    var [etapa4, setEtapa4] = useState(false)
+    const [etapa3, setEtapa3] = useState(false)
+    const [etapa2, setEtapa2] = useState(false)
+    const [etapa1, setEtapa1] = useState(false)
 
 
-    }
-    function e2a() {
-        e12a.classList.toggle(`${stylesMobile.bl}`)
-        e12b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.remove(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.ordem}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    function e3a() {
-        e13a.classList.toggle(`${stylesMobile.bl}`)
-        e13b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.remove(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    function e4a() {
-        e14a.classList.toggle(`${stylesMobile.bl}`)
-        e14b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.remove(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.ordem}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    function e5a() {
-        e15a.classList.toggle(`${stylesMobile.bl}`)
-        e15b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.remove(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.ordem}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    function e6a() {
-        e16a.classList.toggle(`${stylesMobile.bl}`)
-        e16b.classList.toggle(`${stylesMobile.blb}`)
-        oi1.classList.toggle(`${stylesMobile.none}`)
-        oi2.classList.toggle(`${stylesMobile.none}`)
-        oi3.classList.toggle(`${stylesMobile.none}`)
-        oi4.classList.toggle(`${stylesMobile.none}`)
-        oi5.classList.toggle(`${stylesMobile.none}`)
-        oi6.classList.remove(`${stylesMobile.none}`)
-        oi6.classList.toggle(`${stylesMobile.ordem}`)
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
 
-    const boxes = document.querySelectorAll(`.${stylesMobile.drop18}`);
-    let isBoxHidden = false;
-
-    boxes.forEach((box) => {
-        box.addEventListener('click', () => {
-            // se as divs estão visíveis, esconde todas e move a clicada para o topo
-            if (!isBoxHidden) {
-                boxes.forEach((box2) => {
-                    if (box2 !== box) {
-                        box2.style.display = stylesMobile.none;
-                    }
-                });
-                box.style.order = '-1';
-                isBoxHidden = true;
-            } else { // se as divs estão escondidas, mostra todas
-                boxes.forEach((box2) => {
-                    box2.style.display = 'block';
-                    box2.style.order = 'initial';
-                });
-                isBoxHidden = false;
-            }
-        });
-    });
     return (
         <div>
-
+            <Header />
             <section className={`${stylesMobile.sec} ${stylesDesktop.sec}`}>
 
                 <div className={`${stylesMobile.drop1} ${stylesDesktop.drop1}`} id="drop">
                     <div className={`${stylesMobile.drop17} ${stylesDesktop.drop17}`}>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e6a}>
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa6(!etapa6) }}>
+
                             <h1>Etapa 6 <p>A definir</p>
                             </h1>
-                            <div className={`${stylesMobile.tabela_6} ${stylesDesktop.tabela_6}`}>
+                            <div className={`${stylesMobile.tabela_6} ${stylesDesktop.tabela_6}`} style={{ display: etapa6 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele6" className={`${stylesMobile.tabelae6} ${stylesDesktop.tabelae6}`}>
 
                                 </table>
                             </div>
 
-                            <div className={`${stylesMobile.tabela7} ${stylesDesktop.tabela7}`}>
+                            <div className={`${stylesMobile.tabela7} ${stylesDesktop.tabela7}`} style={{ display: etapa6 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavel7" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
 
@@ -153,17 +41,17 @@ function GeneralPoints() {
                                 </table>
                             </div>
                         </div>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e5a}>
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa5(!etapa5) }}>
                             <h1>Etapa 5 <p>A definir</p>
                             </h1>
-                            <div className={`${stylesMobile.tabela_5} ${stylesDesktop.tabela_5}`}>
+                            <div className={`${stylesMobile.tabela_5} ${stylesDesktop.tabela_5}`} style={{ display: etapa5 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele5" className={`${stylesMobile.tabelae5} ${stylesDesktop.tabelae5}`}>
 
                                 </table>
                             </div>
 
-                            <div className={`${stylesMobile.tabela6} ${stylesDesktop.tabela6}`}>
+                            <div className={`${stylesMobile.tabela6} ${stylesDesktop.tabela6}`} style={{ display: etapa5 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavel6" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
 
@@ -171,16 +59,16 @@ function GeneralPoints() {
                                 </table>
                             </div>
                         </div>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e4a}>
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa4(!etapa4) }}>
                             <h1>Etapa 4 <p>A definir</p>
                             </h1>
-                            <div className={`${stylesMobile.tabela_4} ${stylesDesktop.tabela_4}`}>
+                            <div className={`${stylesMobile.tabela_4} ${stylesDesktop.tabela_4}`} style={{ display: etapa4 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele4" className={`${stylesMobile.tabelae4} ${stylesDesktop.tabelae4}`}>
 
                                 </table>
                             </div>
-                            <div className={`${stylesMobile.tabela5} ${stylesDesktop.tabela5}`}>
+                            <div className={`${stylesMobile.tabela5} ${stylesDesktop.tabela5}`} style={{ display: etapa4 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavel5" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
 
@@ -190,17 +78,17 @@ function GeneralPoints() {
 
 
                         </div>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e3a}>
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa3(!etapa3) }}>
                             <h1>Etapa 3 <p>A definir</p>
                             </h1>
-                            <div className={`${stylesMobile.tabela_3} ${stylesDesktop.tabela_3}`}>
+                            <div className={`${stylesMobile.tabela_3} ${stylesDesktop.tabela_3}`} style={{ display: etapa3 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele3" className={`${stylesMobile.tabelae3} ${stylesDesktop.tabelae3}`}>
 
                                 </table>
 
                             </div>
-                            <div className={`${stylesMobile.tabela4} ${stylesDesktop.tabela4}`}>
+                            <div className={`${stylesMobile.tabela4} ${stylesDesktop.tabela4}`} style={{ display: etapa3 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavel4" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
 
@@ -208,10 +96,10 @@ function GeneralPoints() {
                                 </table>
                             </div>
                         </div>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e2a}>
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa2(!etapa2) }}>
                             <h1>Etapa 2 <p>Rio das Ostras</p>
                             </h1>
-                            <div className={`${stylesMobile.tabela_2} ${stylesDesktop.tabela_2}`}>
+                            <div className={`${stylesMobile.tabela_2} ${stylesDesktop.tabela_2}`} style={{ display: etapa2 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele2" className={`${stylesMobile.tabelae2} ${stylesDesktop.tabelae2}`}>
                                     <tr>
@@ -300,7 +188,7 @@ function GeneralPoints() {
                                     </tr>
                                 </table>
                             </div>
-                            <div className={`${stylesMobile.tabela3} ${stylesDesktop.tabela3}`}>
+                            <div className={`${stylesMobile.tabela3} ${stylesDesktop.tabela3}`} style={{ display: etapa2 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavel3" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
                                     <tr>
@@ -394,12 +282,13 @@ function GeneralPoints() {
                                 </table>
                             </div>
                         </div>
-                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={e1a}>
+
+                        <div className={`${stylesMobile.drop18} ${stylesDesktop.drop18}`} onClick={() => { setEtapa1(!etapa1) }}>
                             <h1>Etapa 1 <p>Cabo Frio</p>
                             </h1>
 
 
-                            <div className={`${stylesMobile.tabela_7} ${stylesDesktop.tabela_7}`}>
+                            <div className={`${stylesMobile.tabela_7} ${stylesDesktop.tabela_7}`} style={{ display: etapa1 ? 'block' : 'none' }}>
                                 <h1>Categoria A</h1>
                                 <table id="tabelaNaoEditavele7" className={`${stylesMobile.tabelae7} ${stylesDesktop.tabelae7}`}>
                                     <tr>
@@ -465,7 +354,7 @@ function GeneralPoints() {
                                 </table>
                             </div>
 
-                            <div className={`${stylesMobile.tabela8} ${stylesDesktop.tabela8}`}>
+                            <div className={`${stylesMobile.tabela8} ${stylesDesktop.tabela8}`} style={{ display: etapa1 ? 'block' : 'none' }}>
                                 <h1>Categoria B</h1>
                                 <table id="tabelaNaoEditavele8" className={`${stylesMobile.tabela1} ${stylesDesktop.tabela1}`}>
                                     <tr>
